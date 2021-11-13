c5fadcf15a1467c8be76d7e98c3a521242bce5d4
paul <paulbrowne2@gmail.com>
2021-11-13 11:05:09 +0000

    Created list and detail for comments in views

--- backend/breast_check_api/user_profile/views.py
+++ backend/breast_check_api/user_profile/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics
 from user_profile import serializers
 from django.contrib.auth.models import User
+from user_profile.models import Comment
 
 class UserList(generics.ListAPIView):
     queryset = User.objects.all()
@@ -9,3 +10,14 @@ class UserList(generics.ListAPIView):
 class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = serializers.UserSerializer
+
+class CommentList(generics.ListCreateAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = serializers.CommentSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = serializers.CommentSerializer
