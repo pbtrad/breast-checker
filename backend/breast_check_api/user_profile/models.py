c670af9bde9fb478381b661606a0508a15f2e602
paul <paulbrowne2@gmail.com>
2021-11-13 10:24:51 +0000

    Added views for user profile

--- backend/breast_check_api/user_profile/models.py
+++ backend/breast_check_api/user_profile/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class User_profile(models.Model):
+    user = models.ForeignKey(User, models.CASCADE)
+    name = models.CharField(max_length=100)
+    email = models.EmailField(null=True, blank=True)
+
+    def __str__(self):
+        return "%s - %s" % (self.name, self.email, )
