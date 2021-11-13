afd8cf2d97a951c16f79288643b4390d06f3089f
paul <paulbrowne2@gmail.com>
2021-11-13 11:02:41 +0000

    Added comment serializer

--- backend/breast_check_api/user_profile/serializers.py
+++ backend/breast_check_api/user_profile/serializers.py
@@ -1,7 +1,17 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from user_profile.models import Comment
+
+class CommentSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = Comment
+        fields = ['id', 'title', 'body', 'owner']
 
 class UserSerializer(serializers.ModelSerializer):
+    comments = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'password']
+        fields = ['id', 'username', 'email', 'password', 'comments']
