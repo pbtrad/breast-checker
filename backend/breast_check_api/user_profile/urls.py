59a077c01b24cc248d0b41d4ce5f532fef37641e
paul <paulbrowne2@gmail.com>
2021-11-13 11:17:49 +0000

    set up permissions for security

--- backend/breast_check_api/user_profile/urls.py
+++ backend/breast_check_api/user_profile/urls.py
@@ -5,6 +5,8 @@ from user_profile import views
 urlpatterns = [
     path('users/', views.UserList.as_view()),
     path('users/<int:pk>/', views.UserDetail.as_view()),
+    path('comments/', views.CommentList.as_view()),
+    path('comments/<int:pk>/', views.CommentDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
