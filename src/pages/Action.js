560b4ef43221fe828986c5269cd646970a4ef0dc
Kay Alvaer <mosarwe@yahoo.co.uk>
2021-11-14 18:13:53 +0100

    Added Action page code

---
+++ src/pages/Action.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Action from '../components/Action/Action';
+
+const Home = () => {
+    return (
+      <>
+            <Action />
+      </>
+    );
+};
+
+export default Home;
