9549f6fbd65d243ca151e7e18d6eff2d1b98fe7e
paul <paulbrowne2@gmail.com>
2021-11-14 21:36:34 +0000

    added google auth

--- backend/breast_check_api/user_profile/urls.py
+++ backend/breast_check_api/user_profile/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
-from user_profile.views import RegisterView
+from user_profile.views import RegisterView, GoogleLogin
 from user_profile import views
 from django.db import router
-
 
 urlpatterns = [
     path('users/', views.UserList.as_view()),
@@ -11,7 +10,8 @@ urlpatterns = [
     path('comments/', views.CommentList.as_view()),
     path('comments/<int:pk>/', views.CommentDetail.as_view()),
     #path('register/', views.UserCreate.as_view()),
-    path('accounts/', views.UserCreate.as_view()),
+    path('user-create/', views.UserCreate.as_view()),
+    path('auth/google', GoogleLogin.as_view(), name='google_login'),
 ]
 
 
