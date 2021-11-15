36873c863d68375da6dc4a9e000df257beff9db6
Kay Alvaer <mosarwe@yahoo.co.uk>
2021-11-15 14:51:08 +0100

    Added nav changes

--- frontend/src/components/Header/Navigation.js
+++ frontend/src/components/Header/Navigation.js
@@ -12,31 +12,22 @@ function Navigation() {
       </button>
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav mr-auto">
-          <li className="nav-item active">
-            <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">Link</a>
-          </li>
-          <li className="nav-item dropdown">
-            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              Dropdown
-            </a>
-            <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-              <a className="dropdown-item" href="#">Action</a>
-              <a className="dropdown-item" href="#">Another action</a>
-              <div className="dropdown-divider" />
-              <a className="dropdown-item" href="#">Something else here</a>
-            </div>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link disabled" href="#">Disabled</a>
-          </li>
-        </ul>
-        <form className="form-inline my-2 my-lg-0">
+        <form className="form-inline my-2 my-lg-0 pl-5">
           <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
           <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
         </form>
+        </ul>
+        
+        <li className="nav-item active">
+            <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/checks">Trekker</a>
+          </li>
+    
+          <li className="nav-item">
+            <a className="nav-link disabled" href="http://localhost:3000/login">Login</a>
+          </li>
       </div>
     </nav>
     <Outlet/>
