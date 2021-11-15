966e7f15715aca0509d69bfd85610060411cac5f
Kay Alvaer <mosarwe@yahoo.co.uk>
2021-11-15 01:10:16 +0100

    Added changes to react router and pages

--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
-// import { BrowseRouter as Router, Route, Routes } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Hero from './components/Hero/Hero';
-import Header from './components/Header/Header';
-import Action from './components/Action/Action';
-import Footer from './components/Footer/Footer';
 import "../src/sass/style.scss";
 import './GlobalStyles.css';
+import LoginPage from './pages/Login';
+import RegisterPage from './pages/Register';
+import ActionPage from './pages/Action';
+import CompletePage from './pages/Complete';
+import ManagePage from './pages/Manage';
+import ProfilePage from './pages/profile';
+import TrekkerPage from './pages/Trekker';
+import Navigation from './components/Header/Navigation';
+import HomePage from './pages/Home';
 
 
 function App() {
   return (
-    <div className='Container'>
-      <div className='wrapper'>
-        <Header />
-        <Hero />
-        <Action />
-      </div>
-      <h1>Be Yo Tracker</h1>
-        <div className='Layout'>
-            <h3>Main content</h3>
-            <p>second content</p>
-        </div>
-        <Footer />
-    </div>
+    <>
+    <Routes>
+      <Route path='/' element={<Navigation/>}>
+        <Route path='' element={<HomePage/>}/>
+        <Route path='/login' element={<LoginPage/>}/>
+        <Route path='/register' element={<RegisterPage/>}/>
+        <Route path='/action' element={<ActionPage/>}/>
+        <Route path='/complete' element={<CompletePage/>}/>
+        <Route path='/manage' element={<ManagePage/>}/>
+        <Route path='/profile' element={<ProfilePage/>}/>
+        <Route path='/trekker' element={<TrekkerPage/>}/>
+      </Route>
+      </Routes>
+    </>
   );
 }
 
