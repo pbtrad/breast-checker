cc688ceeb4c8c1a6fe5814e0d036f9faa3cb9f1a
ianmeigh <ianmeigh@googlemail.com>
2021-11-15 13:08:19 +0000

    Add CheckComplete and FurtherAction Pages

---
+++ frontend/src/pages/CheckComplete.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import CheckComplete from '../components/CheckComplete/CheckComplete';
+import Navigation from '../components/Header/Navigation';
+
+const CheckCompletePage = () => {
+    return (
+      <>
+      <CheckComplete />
+      </>
+    );
+};
+
+export default CheckCompletePage;
