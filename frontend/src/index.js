3c762527f6494f570ef3860cea54fe07d7c5b4a7
paul <paulbrowne2@gmail.com>
2021-11-16 08:20:35 +0000

    added basename to Router

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './GlobalStyles.css';
 
 import App from './App';
+
+ReactDOM.render(
+<Router basename={process.env.PUBLIC_URL}>
+  < App />
+  </Router>, document.getElementById('root'));
+
 
 // const routing = (
 //   <BrowserRouter>
@@ -16,11 +22,11 @@ import App from './App';
 // ReactDOM.render(routing,document.getElementById('root'));
 
 
- ReactDOM.render(
-   <BrowserRouter>
-     <App />
-   </BrowserRouter>,
-     document.getElementById('root')
- );
+ //ReactDOM.render(
+   //<BrowserRouter>
+     //<App />
+   //</BrowserRouter>,
+     //document.getElementById('root')
+ //);
 
 
